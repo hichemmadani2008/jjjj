3e886480f3068e3820a940f808a17a7143342bcc
hichemmadani2008 <133526871+hichemmadani2008@users.noreply.github.com>
2023-05-19 02:43:48 +0200

    Add files via upload

---
+++ papa.js
@@ -0,0 +1,117 @@
+const myCc= document.querySelector("#myCc");
+const tt= document.querySelector("#tt");
+const myAa= document.querySelector("#myAa");
+const dd= document.querySelector("#dd");
+const myBb= document.querySelector("#myBb");
+const hh= document.querySelector("#hh");
+const myMom= document.querySelector("#myMom");
+const bb= document.querySelector("#bb");
+const myAunt= document.querySelector("#myAunt");
+const ll= document.querySelector("#ll");
+const myUncle= document.querySelector("#myUncle");
+const mm= document.querySelector("#mm");
+const myDad= document.querySelector("#myDad");
+const ss= document.querySelector("#ss");
+const myEe= document.querySelector("#myEe");
+const vv= document.querySelector("#vv");
+const myFf= document.querySelector("#myFf");
+const zz= document.querySelector("#zz");
+
+
+
+myCc.addEventListener("click", () => {
+
+    if(tt.style.display == "none"){
+        tt.style.display= "block";
+    }
+    else{
+        tt.style.display = "none";
+    }
+    
+})
+myAa.addEventListener("click", () => {
+
+    if(dd.style.display == "none"){
+        dd.style.display = "block";
+    }
+    else{
+        dd.style.display = "none";
+    }
+    
+})
+myBb.addEventListener("click", () => {
+
+    if(hh.style.display == "none"){
+        hh.style.display = "block";
+    }
+    else{
+        hh.style.display = "none";
+    }
+    
+})
+myMom.addEventListener("click", () => {
+
+    if(bb.style.display == "none"){
+        bb.style.display = "block";
+    }
+    else{
+        bb.style.display = "none";
+    }
+    
+})
+myAunt.addEventListener("click", () => {
+
+    if(ll.style.display == "none"){
+        ll.style.display = "block";
+    }
+    else{
+        ll.style.display = "none";
+    }
+    
+})
+myUncle.addEventListener("click", () => {
+
+    if(mm.style.display == "none"){
+        mm.style.display = "block";
+    }
+    else{
+        mm.style.display = "none";
+    }
+    
+})
+
+
+myDad.addEventListener("click", () => {
+
+    if(ss.style.display == "none"){
+        ss.style.display = "block";
+    }
+    else{
+        ss.style.display = "none";
+    }
+    
+})
+
+myEe.addEventListener("click", () => {
+
+    if(vv.style.visibility == "none"){
+        vv.style.visibility = "block";
+    }
+    else{
+      
+        vv.style.visibility = "none";
+    }
+    
+})
+
+myFf.addEventListener("click", () => {
+
+    if(zz.style.visibility == "none"){
+        zz.style.visibility = "block";
+    }
+    else{
+      
+        zz.style.visibility = "none";
+    }
+    
+})
